8437ad040cfd8d0e621b4462b7fbb87c29d348fb
bahoss <bahobek@gmail.com>
2019-12-03 18:21:44 +0600

    Updates

---
+++ precache-manifest.a75377572d541f58ab3f04b3b1979783.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3553d546815a2b497b41b3be6920b7e1",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "6552587f4ebceda948b6",
+    "url": "/deploy/static/css/main.1fce604d.chunk.css"
+  },
+  {
+    "revision": "3f917ec71a8adbecdf57",
+    "url": "/deploy/static/js/2.688e8bff.chunk.js"
+  },
+  {
+    "revision": "6552587f4ebceda948b6",
+    "url": "/deploy/static/js/main.95945ad9.chunk.js"
+  },
+  {
+    "revision": "e409da658de830077a86",
+    "url": "/deploy/static/js/runtime-main.e28825c6.js"
+  },
+  {
+    "revision": "10ec5857cc6eb9dd62f4756e1c5c715d",
+    "url": "/deploy/static/media/CeraPro-Medium.10ec5857.woff"
+  },
+  {
+    "revision": "2bdb9b4e5a1a9766aff6bc07eef79f3d",
+    "url": "/deploy/static/media/CeraPro-Medium.2bdb9b4e.ttf"
+  },
+  {
+    "revision": "561288a6b423c7c1faac8255747dd732",
+    "url": "/deploy/static/media/CeraPro-Medium.561288a6.woff2"
+  },
+  {
+    "revision": "f20f166203c557bd445d652ff12c9a5f",
+    "url": "/deploy/static/media/CeraPro-Medium.f20f1662.eot"
+  },
+  {
+    "revision": "2558fd3ae2d244ceab53b6b455731490",
+    "url": "/deploy/static/media/CeraPro-Regular.2558fd3a.woff"
+  },
+  {
+    "revision": "ae7e6761d3dc3ecd25efdbdb83d68206",
+    "url": "/deploy/static/media/CeraPro-Regular.ae7e6761.eot"
+  },
+  {
+    "revision": "bd818dce28b0e42919636be4fd356e51",
+    "url": "/deploy/static/media/CeraPro-Regular.bd818dce.woff2"
+  },
+  {
+    "revision": "cf954f65e33513f8a1f75f8083674834",
+    "url": "/deploy/static/media/CeraPro-Regular.cf954f65.ttf"
+  },
+  {
+    "revision": "27acd93ce4a49d9cb109804aa914677b",
+    "url": "/deploy/static/media/CeraPro-RegularItalic.27acd93c.woff"
+  },
+  {
+    "revision": "4a1fb48b778ea171e643ddbc237661dd",
+    "url": "/deploy/static/media/CeraPro-RegularItalic.4a1fb48b.woff2"
+  },
+  {
+    "revision": "7a9708efb5f4a9e9676dbb3683edafab",
+    "url": "/deploy/static/media/CeraPro-RegularItalic.7a9708ef.ttf"
+  },
+  {
+    "revision": "bcf83f95da81cd761b3f4a0745fe83f8",
+    "url": "/deploy/static/media/CeraPro-RegularItalic.bcf83f95.eot"
+  },
+  {
+    "revision": "52fdfa1bbec484a305761972ea20d7f2",
+    "url": "/deploy/static/media/authBg.52fdfa1b.png"
+  },
+  {
+    "revision": "640894a18b0250324d08c840a7c9dbe9",
+    "url": "/deploy/static/media/logo.640894a1.svg"
+  }
+]);
